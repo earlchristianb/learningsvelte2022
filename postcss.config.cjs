c24b96aa0daf2523d9b53dbd1013e2bff103107e
Earl Bernardo <35775321+earlchristianb@users.noreply.github.com>
2022-07-31 22:27:18 +0800

    Initial commit

---
+++ postcss.config.cjs
@@ -0,0 +1,9 @@
+/** @format */
+
+import tailwind from "tailwindcss";
+import tailwindConfig from "./tailwind.config.cjs";
+import autoprefixer from "autoprefixer";
+
+export default {
+	plugins: [tailwind(tailwindConfig), autoprefixer],
+};
---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+ <main class="text-red-500">
+  Hello this is good
+</main>
+<p class="bg-black h-10 w-24"></p>
+
