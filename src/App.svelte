aa63c17f0e91b23d8905d97be9ebba184edc5eb7
earlchristianb <35775321+earlchristianb@users.noreply.github.com>
2022-07-31 22:50:03 +0800

    [Earl] Updated tailwind config

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,7 @@
 <script>
 </script>
 
- <main class="text-red-500">
-  Hello this is good
-</main>
+ <main class="text-red-500 flex justify-center items-center w-full h-screen">
 <p class="bg-black h-10 w-24"></p>
-
+<p class="h-20 w-20 rounded-md shadow-md border border-gray-50 py-4 px-2 text-center overflow-auto scroll-pt-10">Yea it is hela good</p>
+</main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,11 @@
 /** @format */
 
-module.exports= {
+module.exports = {
 	plugins: [],
+	content: ["./src/**/*.{html,js,svelte,ts}"],
 	theme: {
 		extend: {},
 	},
-	purge: ["./index.html", "./src/**/*.{svelte,js,ts}"], // for unused CSS
 	variants: {
 		extend: {},
 	},
